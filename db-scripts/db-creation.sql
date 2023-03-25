7339cb9bf22f2b36c966a78ee530d1664d6a8454
kalaitzisxristos <kalaitzisxristos@hotmail.com>
2023-03-25 13:09:43 +0200

    add db-scripts (that will have to run at Supabase)

---
+++ db-scripts/db-creation.sql
@@ -0,0 +1,330 @@
+-- DROP ALL TABLES BEFORE CREATION
+DROP TABLE IF EXISTS canbus_data;
+DROP TABLE IF EXISTS canbus_timeline;
+DROP TABLE IF EXISTS lap;
+DROP TABLE IF EXISTS season;
+DROP TABLE IF EXISTS session;
+DROP TABLE IF EXISTS event_date;
+DROP TABLE IF EXISTS racetrack;
+
+DROP TABLE IF EXISTS message;
+DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS roles;
+
+CREATE TABLE season (
+	id				SERIAL			PRIMARY KEY,
+	name			VARCHAR(150)	NOT NULL,
+	description		TEXT			NOT NULL
+);
+comment on table season is 'The season of the team ex. `2022-2023` or maybe MotoStudent VII';
+comment on column season.name is 'The name of the season ex. MotoStudent VII';
+comment on column season.description is 'Description about the season ex. `Motostudent VII is taking place at Aragon Circuit at 11-14 of Octomber 2023`';
+
+CREATE TABLE event_date (
+	id				SERIAL		PRIMARY KEY,
+	date			timestamp	NOT NULL,
+	description		TEXT		NOT NULL
+);
+comment on table event_date is 'The ';
+comment on column event_date.date is 'The date ex. 14/02/2023';
+comment on column event_date.description is 'Description about this date ex. First day for practice and fine tuning';
+
+CREATE TABLE racetrack (	
+	id				SERIAL			PRIMARY KEY,
+	name			VARCHAR(100)	NOT NULL,
+	country			VARCHAR(10)		NOT NULL,
+	country_code	VARCHAR(10)		NOT NULL
+);
+comment on table racetrack is 'The racetrack that the event is taking place';
+comment on column racetrack.name is 'The name of the racetrack ex. Aragon';
+comment on column racetrack.country is 'The country of the racetrack ex. Spain';
+comment on column racetrack.country_code is 'The code to refence the country of the racetrack ex. es';
+
+CREATE TABLE session (
+	id				SERIAL 		PRIMARY KEY,
+	racetrack_id	INT			NOT NULL,
+	session_order	VARCHAR(10)	NOT NULL,
+	type			VARCHAR(10)	NOT NULL,
+	event_date_id  	INT			NOT NULL,
+		CONSTRAINT fk_racetrack
+		FOREIGN KEY(racetrack_id) 
+		REFERENCES racetrack(id)
+		ON DELETE CASCADE,
+		CONSTRAINT fk_event_date
+		FOREIGN KEY(event_date_id) 
+		REFERENCES event_date(id)
+		ON DELETE CASCADE
+);
+comment on table session is 'The session of the day. In one maybe there are three or more sessions';
+comment on column session.racetrack_id is 'The id that references the recetrack info ex. 12 (id)';
+comment on column session.session_order is 'The order that this session happend ex. session 2';
+comment on column session.type is 'The type of the session ex. practice';
+
+CREATE TABLE lap (
+	id				SERIAL		PRIMARY KEY,
+	session_id		INT			NOT NULL,
+	lap_order		VARCHAR(10)	NOT NULL,
+	created_at		TIMESTAMPTZ	NOT NULL DEFAULT NOW(),
+	CONSTRAINT fk_session
+		FOREIGN KEY(session_id) 
+		REFERENCES session(id)
+		ON DELETE CASCADE
+);
+comment on table lap is 'The lap is the lower level of the hierarchy, holds basic info about the lap such as where (racetrack_id) and when (session_id)';
+comment on column lap.session_id is 'The id that references the session info ex. 502 (id)';
+comment on column lap.lap_order is 'The order of the lap ex. lap 8. If is 0 that means that the laps could not be seperated.';
+comment on column lap.created_at is 'The time of the creation of the row';
+
+CREATE TABLE canbus_timeline (
+	id				SERIAL		PRIMARY KEY,
+	lap_id			INT			NOT NULL,
+	timestamp		TIMESTAMPTZ	NOT NULL,
+	CONSTRAINT fk_lap
+		FOREIGN KEY(lap_id)
+		REFERENCES lap(id)
+		ON DELETE CASCADE
+);
+comment on table canbus_timeline is 'The canbus timeline that has been recored at the lap';
+comment on column canbus_timeline.lap_id is 'The id that references the exact lap.';
+comment on column canbus_timeline.timestamp is 'The time of the value ex. 15:02:46.310 | one to many with the canbus_data';
+CREATE INDEX index_timestamp
+ON canbus_timeline (timestamp);
+
+CREATE TABLE canbus_data (
+	id					SERIAL			PRIMARY KEY,
+	canbus_timeline_id	INT				NOT NULL,
+	canbus_id			VARCHAR(16)		NOT NULL,
+	value				DECIMAL			NOT NULL,
+	canbus_id_name		VARCHAR(120)	NOT NULL,
+	unit				VARCHAR(64)		NOT NULL,
+	CONSTRAINT fk_canbus_timeline
+		FOREIGN KEY(canbus_timeline_id) 
+		REFERENCES canbus_timeline(id)
+		ON DELETE CASCADE
+);
+comment on table canbus_data is 'Based on the time referenced at `canbus_timeline_id` it stores a pair of canbus_id and a value, ex. tire temperature has `15E` as canbus_id and a value of 45°C';
+comment on column canbus_data.canbus_timeline_id is 'The reference to the time of the value ex. 150 (is just an id)';
+comment on column canbus_data.canbus_id is 'The id that the logger uses ex. 15E';
+comment on column canbus_data.value is 'The value that is written at this timestamp for this canbus_id ex. 45.2';
+comment on column canbus_data.canbus_id_name is 'The name if the canbus id ex. for 15E we have a name of `Tire temperature`';
+comment on column canbus_data.unit is 'The measurement unit that will be used to present the value ex. C for Celcius';
+CREATE INDEX index_canbus_id
+ON canbus_data (canbus_id);
+CREATE INDEX index_canbus_id
+ON canbus_data (canbus_id_name);
+
+CREATE TABLE users (
+	id			SERIAL	PRIMARY KEY,
+	uuid		UUID,
+	email		TEXT,
+	password	TEXT,
+	provider	TEXT,
+	created_at	TIMESTAMPTZ	DEFAULT NOW(),
+	full_name	TEXT		DEFAULT 'Firstname_Lastname',
+	about		TEXT		DEFAULT 'about_section',
+	role		TEXT		DEFAULT 'no_role',
+	department	TEXT		DEFAULT 'department',
+	active		BOOLEAN		DEFAULT TRUE
+);
+comment on table users is 'Stores all users registered at the application';
+comment on column users.uuid is 'Supabase assigned unique id. The relation to auth.users.uuid has to be done from Supabase platform.';
+comment on column users.email is 'User email';
+comment on column users.password is 'User password';
+comment on column users.provider is 'Where the user is registered from';
+comment on column users.created_at is 'Timestamp of the creation of the user';
+comment on column users.full_name is 'Full name of the user';
+comment on column users.about is 'Some words as a description for the profile of the user';
+comment on column users.role is 'The active role of the user s department';
+comment on column users.department is 'The department of the user';
+comment on column users.active is 'If is active its true, if he is not its false';
+
+CREATE TABLE roles (
+	id			SERIAL	PRIMARY KEY,
+	role		TEXT,
+	description	TEXT
+);
+comment on table roles is 'Stores all roles available at the application';
+comment on column roles.role is 'The name of the role, ex. Chief Engineer';
+comment on column roles.description is 'A description of the role and that is capable of, ex. Chief Engineer can see ...';
+
+CREATE TABLE user_roles (
+	id				SERIAL		PRIMARY KEY,
+	user_id			INT,
+	role_id			INT,
+	created_at		TIMESTAMP	DEFAULT NOW(),
+	last_modified	TIMESTAMP	DEFAULT NOW(),
+	CONSTRAINT fk_users
+		FOREIGN KEY(user_id)
+		REFERENCES users(id)
+		ON DELETE NO ACTION,
+	CONSTRAINT fk_roles
+		FOREIGN KEY(role_id)
+		REFERENCES roles(id)
+		ON DELETE NO ACTION
+);
+comment on table user_roles is 'This table is to assign roles, it connects the existing users with the existing roles';
+comment on column user_roles.user_id is 'The reference to the user by id';
+comment on column user_roles.role_id is 'The reference to the role by id';
+
+
+DROP TABLE IF EXISTS message;
+DROP TABLE IF EXISTS channel_users;
+DROP TABLE IF EXISTS channel;
+
+
+CREATE TABLE channel (
+	id			SERIAL		PRIMARY KEY,
+	created_at	TIMESTAMP	DEFAULT NOW(),
+	name		VARCHAR(250)
+);
+comment on table channel is 'This table stores all channels';
+comment on column channel.created_at is 'Timestamp of the creation of the channel';
+comment on column channel.name is 'The name of the channel';
+
+CREATE TABLE message (
+	id			SERIAL		PRIMARY KEY,
+	created_at	TIMESTAMP	DEFAULT NOW(),
+	content		TEXT,
+	user_id		INT,
+	channel_id	INT,
+	type		TEXT,
+	CONSTRAINT fk_users
+		FOREIGN KEY(user_id)
+		REFERENCES users(id)
+		ON DELETE NO ACTION,
+	CONSTRAINT fk_channel
+		FOREIGN KEY(channel_id)
+		REFERENCES channel(id)
+		ON DELETE NO ACTION
+);
+comment on table message is 'This table stores all messages';
+comment on column message.created_at is 'Timestamp of the creation of the message';
+comment on column message.content is 'The content of the message';
+comment on column message.user_id is 'The reference to the user by id';
+comment on column message.channel_id is 'The reference to the channel by id';
+comment on column message.type is 'The type of the message';
+
+CREATE TABLE channel_users (
+	id			SERIAL		PRIMARY KEY,
+	created_at	TIMESTAMP	DEFAULT NOW(),
+	user_id		INT,
+	channel_id	INT,
+	CONSTRAINT fk_users
+		FOREIGN KEY(user_id)
+		REFERENCES users(id)
+		ON DELETE NO ACTION,
+	CONSTRAINT fk_channel
+		FOREIGN KEY(channel_id)
+		REFERENCES channel(id)
+		ON DELETE NO ACTION
+);
+comment on table channel_users is 'This table stores all users within the channels and vise-versa, all channels of the user';
+comment on column channel_users.created_at is 'Timestamp of the insertion of the user at the channel';
+comment on column channel_users.user_id is 'The reference to the user by id';
+comment on column channel_users.channel_id is 'The reference to the channel by id';
+
+
+
+DROP TABLE IF EXISTS part_values;
+DROP TABLE IF EXISTS part;
+DROP TABLE IF EXISTS system;
+DROP TABLE IF EXISTS vehicle;
+
+CREATE TABLE vehicle (
+	id			SERIAL		PRIMARY KEY,
+	created_at	TIMESTAMP	DEFAULT NOW(),
+	name		VARCHAR(250),
+	year		VARCHAR(16),
+	description	VARCHAR(250)
+);
+
+CREATE TABLE system (
+	id			SERIAL		PRIMARY KEY,
+	created_at	TIMESTAMP	DEFAULT NOW(),
+	name		VARCHAR(250),
+	vehicle_id	INT,
+	description	VARCHAR(250),
+	CONSTRAINT fk_vehicle
+		FOREIGN KEY(vehicle_id)
+		REFERENCES vehicle(id)
+		ON DELETE NO ACTION
+);
+
+CREATE TABLE part (
+	id					SERIAL		PRIMARY KEY,
+	created_at			TIMESTAMP	DEFAULT NOW(),
+	name				VARCHAR(250),
+	system_id			INT,
+	current_value_id	INT,
+	measurement_unit	VARCHAR(250),
+	CONSTRAINT fk_vehicle
+		FOREIGN KEY(system_id)
+		REFERENCES system(id)
+		ON DELETE NO ACTION
+);
+
+CREATE TABLE part_values (
+	id			SERIAL		PRIMARY KEY,
+	created_at	TIMESTAMP	DEFAULT NOW(),
+	part_id		INT,
+	value		INT,
+	CONSTRAINT fk_vehicle
+		FOREIGN KEY(part_id)
+		REFERENCES part(id)
+		ON DELETE NO ACTION
+);
+
+create or replace function public.handle_new_user() 
+returns trigger as $$
+	begin
+		insert into public.users (uuid, email, full_name)
+		values (new.id, new.email, new.raw_user_meta_data->>'full_name');
+		return new;
+	end;
+$$ language plpgsql security definer;
+
+-- trigger the function every time a user is created
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
+
+
+----------------------------------------
+-- initialization of the Database Data--
+----------------------------------------
+
+INSERT INTO roles(role, description)
+VALUES 
+('admin', 'This is the admin role. An admin has access to all users and their roles and all features of the application.'),
+('engineer','This is the engineer role. It is the standard role and unlocks all features of the application.'),
+('data_analyst','This is the data analyst role. This role has only access to data from previous sessions.'),
+('default','This is the default role. This role has no rights in the application.');
+
+-- set the default user as the default id at user creation at user_roles
+ALTER TABLE user_roles
+ALTER COLUMN role_id SET DEFAULT 4;
+
+INSERT INTO users (uuid, email, password, provider, created_at, full_name, about, role, department, active)
+VALUES (null, 'admin', 'admin', 'panther', NOW(), 'administrator', 'Panther s Administrator', 'Engineer','electronics', true),
+(null, 'engineer', 'engineer', 'panther', NOW(), 'engineer', 'Panther s engineer', 'Engineer','electronics', true),
+(null, 'data', 'data', 'data_analyst', NOW(), 'data_analyst', 'Panther s Data Analyst', 'Engineer','frame', true),
+(null, 'default', 'default', 'default', NOW(), 'default', 'Panther s Default User', 'No role','department', true);
+
+INSERT INTO user_roles (user_id, role_id)
+VALUES (1, 1),
+(2,2),
+(3,3),
+(4,4);
+
+INSERT INTO event_date(date, description)
+VALUES (NOW(), 'Finals');
+INSERT INTO racetrack(name, country, country_code)
+VALUES ('Aragon', 'Spain', 'es');
+INSERT INTO session(racetrack_id, session_order, type, event_date_id)
+VALUES (1, '1', 'practice', 1);
+INSERT INTO lap(session_id, lap_order)
+VALUES (1, 1);
+
+
+
